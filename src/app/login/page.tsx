109cac4286ae4169ad95cf669771d6b80bf1a0e4
shahadat <nayemshahadat581@gmail.com>
2025-04-18 16:43:00 +0600

    login page design done

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginPage from "@/components/modules/auth/login/LoginForm"
+
+const Login=()=>{
+    return(
+        <div className="flex min-h-screen items-center justify-center">
+             <LoginPage></LoginPage>
+        </div>
+    )
+}
+export default Login
--- src/components/modules/auth/login/LoginForm.tsx
+++ src/components/modules/auth/login/LoginForm.tsx
@@ -4,50 +4,51 @@ import Logo from "@/app/assets/svgs/Logo"
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { RegisterUser } from '@/services/Authservices'
-import { TUser } from '@/types/type'
+import { motion } from "motion/react"
+
 import Image from 'next/image'
 import Link from 'next/link'
 import {  FieldValues, SubmitHandler, useForm } from "react-hook-form"
 import { FaFacebook } from "react-icons/fa"
 import { toast } from 'sonner'
 
-const RegisterPage = () => {
+const LoginPage = () => {
     const form = useForm()
     const {formState:{isSubmitting}}=form
     const onSubmit:SubmitHandler<FieldValues> =async (data) => {
         
         try{
-             const res=await RegisterUser(data as TUser)
-             if(res.success){
-                toast.success(res.message,{
-                    style:{
-                        background:"white",
-                        color:"green"
-                    },
-                    position:"top-left"
-                })
-             }
-             else{
-                toast.error("Something went wrong",{
-                    style:{
-                        background:"white",
-                        color:"red"
-                    },
-                    position:"top-left"
-                })
-             }
+            console.log(data)
+            //  const res=await RegisterUser(data as TUser)
+            //  if(res.success){
+            //     toast.success(res.message,{
+            //         style:{
+            //             background:"white",
+            //             color:"green"
+            //         },
+            //         position:"top-left"
+            //     })
+            //  }
+            //  else{
+            //     toast.error("Something went wrong",{
+            //         style:{
+            //             background:"white",
+            //             color:"red"
+            //         },
+            //         position:"top-left"
+            //     })
+            //  }
         }
         catch(err){
             console.log(err)
         }
     }
     return (
-        <div className="p-6 rounded-[8px] shadow-2xl w-[360px] md:w-[575px] lg:w-[400px]">
+        <motion.div initial={{scale:0,opacity:0}} animate={{scale:1,opacity:1}} transition={{duration:0.5,delay:0.3}} className="p-6 rounded-[8px] shadow-2xl w-[360px] md:w-[575px] lg:w-[400px]">
             <div className="flex items-center space-x-4 ">
                 <Logo />
                 <div>
-                    <h1 className="text-2xl font-semibold">Register</h1>
+                    <h1 className="text-2xl font-semibold">Login</h1>
                     <p className="text-sm font-medium text-gray-600">
                         Join us today and start your journey!
                     </p>
@@ -55,20 +56,7 @@ const RegisterPage = () => {
             </div>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3 mt-3">
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input className="h-[42px] rounded-none" {...field} value={field.value || ""} />
-                                </FormControl>
-                                
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                  
                     <FormField
                         control={form.control}
                         name="email"
@@ -97,13 +85,24 @@ const RegisterPage = () => {
                             </FormItem>
                         )}
                     />
-                    <Button className="cursor-pointer w-full text-[18px] h-[43px] rounded-none" type="submit">{isSubmitting?"Registering...":"Register"}</Button>
+                    <Button className="cursor-pointer w-full text-[18px] h-[43px] rounded-none" type="submit">{isSubmitting?"Loging...":"Login"}</Button>
                 </form>
             </Form>
             <div>
-                <Link href={'/login'} className='text-center block mt-2'>Already have account?</Link>
+                 
+                 <div className="mt-4">
+                     <button className="cursor-pointer w-full font-medium flex space-x-2 justify-center items-center h-[45px] border-2 border-blue-500">
+                         <FaFacebook className="text-blue-500 text-[32px]"></FaFacebook>
+                         <span className=''>Sign In With Facebook</span>
+                     </button>
+                     <button className="cursor-pointer w-full font-medium flex mt-4 space-x-2 justify-center items-center h-[45px] border-2 border-black">
+                         <Image src={google} height={30} width={30} alt='Google'></Image>
+                         <span className=''>Sign In With Facebook</span>
+                     </button>
+                 </div>
+                 <Link href={'/register'} className='text-center mt-2 block'>Have not any account?</Link>
             </div>
-        </div>
+        </motion.div>
     )
 }
-export default RegisterPage
+export default LoginPage
